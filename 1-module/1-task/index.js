1d84dcf557489e9063605779b142a6de86688931
Olga Kitaeva <olikkitt@gmail.com>
2021-12-24 17:47:33 +0200

    Factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,18 @@
 function factorial(n) {
-  // ваш код...
+  
+  let i = 1, factorial = n;
+
+  if (n == 0) {
+    return 1;
+  }
+  
+  while (i < n) {
+  
+   factorial = factorial * (n - i);
+   i++;
+  }
+
+  return factorial;
 }
+
+console.log (factorial(5));
