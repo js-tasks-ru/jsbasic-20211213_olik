26d7ab11b15805c049fd03430e1ca04374015a9a
Olga Kitaeva <olikkitt@gmail.com>
2021-12-27 17:32:14 +0200

    Empty obj

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,22 @@
 function isEmpty(obj) {
-  // ваш код...
+  let i = 0;
+  for (let key in obj) {
+    i++;
+  }
+  if (i < 1){
+    return true;
+  }
+ //  console.log (i);
+   return false;
 }
+
+let obj = {};
+let obj3 = {age: 12};
+let obj1 = {
+  Name: "Anton",
+  age: 10
+};
+
+console.log("obj " + isEmpty(obj));
+console.log("obj1 " + isEmpty(obj1));
+console.log("obj3 " + isEmpty(obj3));
