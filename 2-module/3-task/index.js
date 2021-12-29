be67cbdf6ee970ad047a8ec0865aa7f0c370a182
Olga Kitaeva <olikkitt@gmail.com>
2021-12-29 17:29:15 +0200

    Change function read

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,17 +1,18 @@
 let calculator = {
   read: function read(a, b) { 
-    a = +prompt("number a", 3);
-    b = +prompt("number b", 5);
+    this.a = a;
+    this.b = b;
    
   },
- sum: (a, b) =>  this.a + this.b,
- mul: (a, b) =>  this.a * this.b,
+  
+ sum() {  return this.a + this.b},
+ mul() {  return this.a * this.b }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
 
-calculator.read(3, 5);
+calculator.read(4, 7);
 console.log("Sum= " + calculator.sum()); 
 console.log("Mult= " + calculator.mul()); 
 
