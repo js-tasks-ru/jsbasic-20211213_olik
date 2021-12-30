87e0b63aebae9785ece110e8fb68f18da7deb950
Olga Kitaeva <olikkitt@gmail.com>
2021-12-30 18:01:32 +0200

    Fillter array

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let result = arr.filter(item => (a <= item && item <= b));
+  return result;
 }
+
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 5);
+
+console.log("filtered = " + filtered ); 
+console.log("arr = " + arr ); 
