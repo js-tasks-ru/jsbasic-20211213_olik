701e2b3ade23fa758bd760024bc25959448bcc21
Olga Kitaeva <olikkitt@gmail.com>
2021-12-30 17:20:30 +0200

    Array of names

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,14 @@
 function namify(users) {
-  // ваш код...
+  let result = users.map(arrNames => arrNames.name ) ;
+  return result;
 }
+
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = namify(users);
+
+console.log(names);
