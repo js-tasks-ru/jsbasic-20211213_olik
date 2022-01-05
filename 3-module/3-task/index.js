29e688419d972dbbe7a72c6b40b82b059e129a4b
olik <kolga210921@gmail.com>
2022-01-06 01:10:45 +0200

    Upper Case

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+
+let result = str.split('-').map(
+     
+      (str, index) => index == 0 ? str : str[0].toUpperCase() + str.slice(1)
+    )
+    .join(''); 
+   return result;
 }
+
+
+console.log(camelize('background-color'));// 'backgroundColor';
+console.log(camelize('list-style-image'));// 'listStyleImage';
+console.log(camelize('-webkit-transition')) ;//'WebkitTransition';
