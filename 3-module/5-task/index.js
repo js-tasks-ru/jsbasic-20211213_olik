45c3f02a8326b0e33ce8433047c0b02590739e27
olik <kolga210921@gmail.com>
2022-01-08 11:14:45 +0200

    Min and Max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,29 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = [], obj = {};
+  obj.min = 0;
+  obj.max = 0;
+  let numItem = 0;
+
+  arr = str.split(" ")
+  .filter(function(el) { return Number(el); });
+
+  arr.forEach(function(item, index) {
+
+    numItem = Number(item);
+
+    if(obj.min > numItem)
+     {
+      obj.min = numItem;
+     }
+
+     if(obj.max < numItem)
+     {
+      obj.max = numItem;
+     }     
+ 
+  });
+  return obj;
 }
+
+const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+console.log(getMinMax(inputData)); // { min: -5.8, max: 73  }
