9c1def0ab228fc2cd08416a69997c228b8d9d5e1
Olga Kitaeva <olikkitt@gmail.com>
2022-01-14 13:10:54 +0200

    Toggle class

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector(".toggle-text-button");
+  let txt = document.querySelector("#text");
+  btn.onclick =  function(){
+    if (!txt.hasAttribute('hidden')){
+      txt.setAttribute('hidden', 'true');
+    } else {
+      txt.removeAttribute('hidden');
+    }
+  }
 }
